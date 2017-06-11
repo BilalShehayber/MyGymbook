625a935946697c4e9ccbc65f84a771177e606d07
Bilal Shehayber <Bilal@bilals-macbook-air.local>
2017-06-10 19:10:53 -0500

    Git set Up

--- MyGymBook/ViewController.swift
+++ MyGymBook/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
 
+    @IBOutlet weak var addWorkout: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: Actions
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func moveToNextView(_ sender: UIButton) {
+        
     }
-
-
+    
 }
 
